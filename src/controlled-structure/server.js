fb5f3b06916b4cfe3d7b6a5c804a1c6925c225b6
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-01 00:55:48 -0300

    add express server

---
+++ src/controlled-structure/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Hello, world!");
+  });
+
+app.listen(PORT, () => {
+  console.log("App listening on port " + PORT);
+});
